ca8e2fe3d5d1c9010badcea1e8aa63da337b9877
zotel1 <zotelsigel@gmail.com>
2025-10-08 20:01:46 -0300

    creamos nuestro primer dto DownloadRequest con su getter and setter

---
+++ src/main/java/com/principal/backend/dto/DownloadRequest.java
@@ -0,0 +1,13 @@
+package com.principal.backend.dto;
+
+public class DownloadRequest {
+    private String url;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
