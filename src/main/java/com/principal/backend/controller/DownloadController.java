3816039fc11ae30150420d90725d5555c034f968
zotel1 <zotelsigel@gmail.com>
2025-10-08 20:12:04 -0300

    terminamos de adaptar nuestro controller

--- src/main/java/com/principal/backend/controller/DownloadController.java
+++ src/main/java/com/principal/backend/controller/DownloadController.java
@@ -1,5 +1,9 @@
 package com.principal.backend.controller;
 
+import com.principal.backend.dto.DownloadRequest;
+import com.principal.backend.dto.DownloadResponse;
+import com.principal.backend.service.DownloadService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,8 +13,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/download")
 public class DownloadController {
 
+    @Autowired
+    private DownloadService downloadService;
+
     @PostMapping
     public DownloadResponse handleDownload(@RequestBody DownloadRequest request){
-        return downloadService.dowloadFromPython(request);
+        return downloadService.downloadFromPython(request);
     }
 }
