7c3e67942bedfa05195cfdc29ed302de8823ff29
zotel1 <zotelsigel@gmail.com>
2025-10-10 00:49:10 -0300

    creamos nuestro dto para nuestro GoogleUploadRequest

---
+++ src/main/java/com/principal/backend/dto/GoogleUploadRequest.java
@@ -0,0 +1,22 @@
+package com.principal.backend.dto;
+
+public class GoogleUploadRequest {
+    private String accessToken;
+    private String filePath;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+}
