8a73078cbd931d86758f93faefc48bfd16e792b3
zotel1 <zotelsigel@gmail.com>
2025-10-12 16:11:22 -0300

    Modificamos nuestro dto DownloadRequest ya qyue el frontend ahora envia dos cosas la url del viodeo y el accessToken

--- src/main/java/com/principal/backend/dto/DownloadRequest.java
+++ src/main/java/com/principal/backend/dto/DownloadRequest.java
@@ -2,6 +2,7 @@ package com.principal.backend.dto;
 
 public class DownloadRequest {
     private String url;
+    private String accessToken;
 
     public String getUrl() {
         return url;
@@ -9,5 +10,13 @@ public class DownloadRequest {
 
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
     }
 }
