20c4319f8ac3a06daf324beb7280968e93f45ee7
zotel1 <zotelsigel@gmail.com>
2026-06-07 03:52:53 -0300

    fix(tests): add MockBean for UserRepository, JwtProvider, and PasswordEncoder to load context

--- src/test/java/com/principal/backend/BackendApplicationTests.java
+++ src/test/java/com/principal/backend/BackendApplicationTests.java
@@ -1,12 +1,25 @@
 package com.principal.backend;
 
+import com.principal.backend.application.port.PasswordEncoder;
+import com.principal.backend.domain.port.JwtProvider;
+import com.principal.backend.domain.port.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest(properties = {
 		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
 })
 class BackendApplicationTests {
+
+	@MockBean
+	private UserRepository userRepository;
+
+	@MockBean
+	private JwtProvider jwtProvider;
+
+	@MockBean
+	private PasswordEncoder passwordEncoder;
 
 	@Test
 	void contextLoads() {
