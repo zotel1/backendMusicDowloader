17b981de901374540155608d2137ae19cdf04497
zotel1 <zotelsigel@gmail.com>
2026-06-07 02:49:14 -0300

    config: add database properties and update gitignore

--- src/test/java/com/principal/backend/BackendApplicationTests.java
+++ src/test/java/com/principal/backend/BackendApplicationTests.java
@@ -1,13 +1,10 @@
 package com.principal.backend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(excludeAutoConfiguration = {
-		DataSourceAutoConfiguration.class,
-		HibernateJpaAutoConfiguration.class
+@SpringBootTest(properties = {
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration"
 })
 class BackendApplicationTests {
 
